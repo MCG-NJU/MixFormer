219bd14704ec217919c3b1eb310940769546c2d6
cuiyutao <cuiyutao@smail.nju.edu.cn>
2022-04-13 15:38:15 +0800

    update run_video_demo

--- tracking/run_video_demo.sh
+++ tracking/run_video_demo.sh
@@ -1,8 +1,7 @@
 # We only support manually setting the bounding box of first frame and save the results in debug directory.
-# We plan to release a colab for running your own video demo in the future.
 
 ##########-------------- MixFormer-22k-----------------##########
-python tracking/video_demo.py mixformer_online baseline /YOURS/VIDEO/PATH  \
+python tracking/video_demo.py mixformer_online baseline /YOUR/VIDEO/PATH  \
    --optional_box [YOURS_X] [YOURS_Y] [YOURS_W] [YOURS_H] --params__model mixformer_online_22k.pth.tar --debug 1 \
   --params__search_area_scale 4.5 --params__update_interval 25 --params__online_sizes 3
 
