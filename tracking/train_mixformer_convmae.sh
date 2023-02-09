ea4a311fb3436718db092dea22ff15cd35b04f58
sth <191098194@smail.nju.edu.cn>
2023-02-09 17:02:35 +0800

    fix typo

--- tracking/train_mixformer_convmae.sh
+++ tracking/train_mixformer_convmae.sh
@@ -1,6 +1,6 @@
 # There are the detailed training settings for MixFormer-Convmae-B and MixFormer-Convmae-L.
-# 1. download pretrained ViT-MAE models (mae_pretrain_vit_base.pth.pth/mae_pretrain_vit_large.pth) at https://github.com/facebookresearch/mae
-# 2. set the proper pretrained CvT models path 'MODEL:BACKBONE:PRETRAINED_PATH' at experiment/mixformer_vit/CONFIG_NAME.yaml.
+# 1. download pretrained ConvMAE models (convmae_base.pth.pth/convmae_large.pth) at https://github.com/Alpha-VL/ConvMAE
+# 2. set the proper pretrained convmae models path 'MODEL:BACKBONE:PRETRAINED_PATH' at experiment/mixformer_convmae/CONFIG_NAME.yaml.
 # 3. uncomment the following code to train corresponding trackers.
 
 ### Training MixFormer-Convmae-B
